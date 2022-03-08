d7a1edb93528290ebd0658f521ef255a79de2fb3
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-08 11:04:39 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+//2.2.1
+//2.2.2
+console.log("Hello World"); 
+
+//2.2.4 セミコロンの自動挿入
+console.log("hello")
+
+
